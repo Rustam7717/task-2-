b8bc5dbe42c1d354a9c03642bec782e64eda07c1
Rustam7717 <dj_rustam85@mail.ru>
2022-09-07 12:46:40 +0600

    homeworktask2

---
+++ check.js
@@ -0,0 +1,13 @@
+const mass1 = prompt('Input number!');
+const mass2 = prompt('Input number to compare')
+if (mass1>mass2){
+    alert('First massive is larger than second one')
+}
+else if(mass1==mass2){
+    alert ('First massive is equal to second one')
+}else{
+(mass1<mass2)
+alert('Second massive is larger than fisrt one')
+}
+
+
